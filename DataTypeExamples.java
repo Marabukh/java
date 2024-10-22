979437f1c483e6063430afbc459c6c7ffab3db21
Марк Бухаров <mbukharov52@gmail.com>
2024-10-22 17:30:13 +0300

    Create DataTypeExamples.java

---
+++ DataTypeExamples.java
@@ -0,0 +1,12 @@
+public class DataTypeExamples {
+	public static void main(String[] args) {
+	    byte byteExample = 127;
+	    short shortExample = 32767;
+	    int intExample = 2147483647;
+	    long longExample = 9223372036854775807l;
+	    float floatExample = 1.876656f;
+	    double doubleExample = 2.987697675656787;
+	    boolean booleanExample = true;
+	    char charExample = 'a';
+	}
+}
