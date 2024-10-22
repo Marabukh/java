a239457162c3059dd4852c371a05231bd127374b
Марк Бухаров <mbukharov52@gmail.com>
2024-10-22 18:07:22 +0300

    Create TypeConversion.java

---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+  	public static void main(String[] args) {
+  	    double a = 2.87686134534;
+  	    float b = (float) a;
+  	    System.out.println(b);
+  	    long c = 1000;
+  	    short d = (short) c;
+  	    System.out.println(d);
+  	}
+}
